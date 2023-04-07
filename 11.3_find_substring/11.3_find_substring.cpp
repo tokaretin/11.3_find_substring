bfa298aa55642a4394c0e1d77b23a2b79db001ce
tokaretin <tokaretin@gmail.com>
2023-04-07 16:02:34 +0200

    Добавьте файлы проекта.

---
+++ 11.3_find_substring/11.3_find_substring.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+/* Поиск подстроки в строке */
+
+bool is_match(std::string text, std::string word, int startPos) {
+    for (int i = 0; i < word.length(); ++i) {
+        if (i + startPos >= text.length() || word[i] != text[i + startPos]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int find(std::string text, std::string word) {
+    for (int startPos = 0; startPos < text.length(); ++startPos) {
+        if (is_match(text, word, startPos)) {
+            return startPos;
+        }
+    }
+    return -1;
+}
+
+void found() { std::cout << "Found!" << std::endl; }
+
+int main() {
+    std::string text;
+    std::string word;
+
+    std::cin >> text;
+    std::cin >> word;
+
+    if (find(text, word) >= 0) {
+        found();
+    }
+}
